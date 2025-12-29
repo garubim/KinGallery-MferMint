3c3dd42d5f6244846930fd37d6ab0dc101dd0eb6
garubim <81217699+garubim@users.noreply.github.com>
2025-12-29 11:14:51 -0300

    Bulk push: arquivos essenciais para KinGallery mini app

---
+++ content/mintText.ts
@@ -0,0 +1,7 @@
+export const mintText = {
+  welcome: 'Welcome, to EternalLoop.',
+  preconnect: "You're early to getEternalLoop(): Connect!",
+  connected: 'function getEternalLoop()',
+};
+
+export default mintText;
